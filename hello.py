f1821facfff05670042efa9e155134ac19f87772
anas.falau <anas.falau@gmail.com>
2018-11-15 09:33:17 +0100

    My first github push

--- hello.py
+++ hello.py
@@ -1,11 +1,8 @@
 #THIS IS SIMPLE GIT TUTORIAL TEST PROGRAM
 
 
-def say_hello():
         
         name = input('Please enter your name: ') or 'ANAS'
         
         print('Hello, ' ,name)
 
-
-say_hello()
